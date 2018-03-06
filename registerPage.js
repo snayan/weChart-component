dc3763676bc25443b668d38080fd10185de05c0a
sanyan <snayan@sina.com>
2018-03-06 17:43:25 +0800

    优化了registerPage

--- registerPage.js
+++ registerPage.js
@@ -3,30 +3,55 @@
 const nativePage = Page;
 
 /* 组件文件夹 */
-const baseComponent = "./components/";
+const BASECOMPONENT = './components/';
+
+/* 受保护的属性 */
+const PROTECTEDPROPERTY = ['name', 'parent', 'data', 'setData'];
+
+/* 受保护的页面事件 */
+const PROTECTEDEVENTS = [
+  'onLoad',
+  'onReady',
+  'onShow',
+  'onHide',
+  'onUnload',
+  'onPullDownRefreash',
+  'onReachBottom',
+  'onPageScroll'
+];
+
+/* 是否包含name */
+function ownHas(container, name) {
+  if (container === null || container === undefined) {
+    return false;
+  }
+  if (Array.isArray(container)) {
+    return !!~container.indexOf(name);
+  }
+  if (typeof container === 'object') {
+    return Object.hasOwnProperty.call(container, name);
+  }
+  return false;
+}
 
 /* 错误消息 */
-const handlerError = (name, type, msg) => {
+function handlerError(name, type, msg) {
   let errors = [msg];
   if (name) {
     errors[1] = `error occur in the ${type} : ${name}`;
   }
-  throw new Error(errors.join("\n"));
-};
+  throw new Error(errors.join('\n'));
+}
 
 /* 解析URL */
-let parseUrl = url => {
-  url = baseComponent + url;
-  if (/.js$/.test(url)) {
-    return [url];
-  } else {
-    return [url + ".js", url + "/index.js"];
-  }
-};
+function parseUrl(url) {
+  let fullUrl = BASECOMPONENT + url;
+  return /.js$/.test(fullUrl) ? [fullUrl] : [fullUrl + '.js', fullUrl + '/index.js'];
+}
 
 /* 替换原值 */
-let assign = (first, second) => {
-  if (typeof second !== "object" || Array.isArray(second) || second === null) {
+function assign(first, second) {
+  if (typeof second !== 'object' || Array.isArray(second) || second === null) {
     return second;
   }
   if (first == null) {
@@ -37,33 +62,33 @@ let assign = (first, second) => {
     first[key] = assign(first[key], second[key]);
   }
   return first;
-};
+}
 
 /* 解析数据 */
-let parseData = (name, originData, data) => {
+function parseData(name, originData, data) {
   let value, type;
   let result = Object.create(null);
   let keys = Object.keys(data);
   for (let key of keys) {
     value = data[key];
     type = typeof value;
-    if (type === "object" && type !== null) {
+    if (type === 'object' && type !== null) {
       result[`${name}.${key}`] = assign(originData[key], value);
     } else {
       result[`${name}.${key}`] = value;
     }
   }
   return result;
-};
+}
 
 /* 引入组件 */
-let requireCo = name => {
+function requireCo(name) {
   if (!name) {
     return null;
   }
   let result, currentUrl;
   let urls = parseUrl(name);
-  let errorUrl = urls.map(v => v.slice(2)).join(",");
+  let errorUrl = urls.map(url => url.slice(2)).join(',');
   while (!result && (currentUrl = urls.shift())) {
     try {
       result = require(currentUrl);
@@ -72,53 +97,34 @@ let requireCo = name => {
     }
   }
   if (!result) {
-    handlerError(
-      null,
-      "component",
-      `import component error :${errorUrl} can not find,please check.`
-    );
+    handlerError(null, 'component', `import component error :${errorUrl} can not find,please check.`);
   }
-  if (result && typeof result.initComponent !== "function") {
-    handlerError(currentUrl, "component", "can not find initComponent.");
+  if (result && typeof result.initComponent !== 'function') {
+    handlerError(currentUrl, 'component', 'can not find initComponent function.');
   }
   try {
     result = result.initComponent();
   } catch (e) {
-    handlerError(currentUrl, "component", e.message);
+    handlerError(currentUrl, 'component', e.message);
   }
   if (!result.name) {
     result.name = name;
   }
   return result;
-};
-
-/* 受保护的属性 */
-const protectedProperty = ["name", "parent", "data", "setData"];
-
-/* 受保护的页面事件 */
-const protectedEvent = [
-  "onLoad",
-  "onReady",
-  "onShow",
-  "onHide",
-  "onUnload",
-  "onPullDownRefreash",
-  "onReachBottom",
-  "onPageScroll"
-];
+}
 
 /* 创建一个新的Component作用域 */
-const createComponentThis = (component, page) => {
-  let name = component.name;
+function createComponentThis(component, page) {
+  let { name } = component;
+  //直接返回缓存的this
   if (page[`__${name}.this__`]) {
     return page[`__${name}.this__`];
   }
   let keys = Object.keys(component);
   let newThis = Object.create(null);
-  let protectedKeys = protectedProperty.concat(protectedEvent);
-  let otherKeys = keys.filter(v => !~protectedKeys.indexOf(v));
+  let otherKeys = keys.filter(key => !ownHas([...PROTECTEDPROPERTY, ...PROTECTEDEVENTS], key));
   for (let key of otherKeys) {
-    if (typeof component[key] === "function") {
+    if (typeof component[key] === 'function') {
       Object.defineProperty(newThis, key, {
         get() {
           return page[`${name}.${key}`];
@@ -138,28 +144,28 @@ const createComponentThis = (component, page) => {
       });
     }
   }
-  Object.defineProperty(newThis, "name", {
+  Object.defineProperty(newThis, 'name', {
     configurable: false,
     enumerable: false,
     get() {
       return name;
     }
   });
-  Object.defineProperty(newThis, "data", {
+  Object.defineProperty(newThis, 'data', {
     configurable: false,
     enumerable: false,
     get() {
       return page.data[name];
     }
   });
-  Object.defineProperty(newThis, "parent", {
+  Object.defineProperty(newThis, 'parent', {
     configurable: false,
     enumerable: false,
     get() {
       return page;
     }
   });
-  Object.defineProperty(newThis, "setData", {
+  Object.defineProperty(newThis, 'setData', {
     value: function(data) {
       page.setData(parseData(name, this.data, data));
     },
@@ -168,99 +174,94 @@ const createComponentThis = (component, page) => {
   });
   page[`__${name}.this__`] = newThis;
   return newThis;
-};
+}
 
 /* 绑定子组件生命周期钩子函数 */
-const bindComponentLifeEvent = page => {
-  let components = page.components;
-  for (let key of protectedEvent) {
-    let symbols = page[Symbol["for"](key)];
-    let pageLifeFn = page[key];
-    if (Array.isArray(symbols) && symbols.length > 0) {
-      if (typeof pageLifeFn === "function") {
-        symbols.push({
-          fn: pageLifeFn,
-          type: "page",
-          context: page
+function bindComponentLifeEvent(page) {
+  let pageData = { ...page };
+  let { components } = pageData;
+  for (let key of PROTECTEDEVENTS) {
+    let childPageLifeFns = pageData[Symbol['for'](key)];
+    let parentPageLifeFn = pageData[key];
+    if (Array.isArray(childPageLifeFns) && childPageLifeFns.length > 0) {
+      if (typeof parentPageLifeFn === 'function') {
+        childPageLifeFns.push({
+          fn: parentPageLifeFn,
+          type: 'page',
+          context: pageData
         });
       }
-      page[key] = function() {
+      pageData[key] = function(...args) {
         let pageThis = this;
-        let args = Array.from(arguments);
-        for (let ofn of symbols) {
+        for (let lifeFn of childPageLifeFns) {
           let currentThis;
-          if (ofn.type === "component") {
-            currentThis = createComponentThis(ofn.context, pageThis);
+          if (lifeFn.type === 'component') {
+            currentThis = createComponentThis(lifeFn.context, pageThis);
           } else {
             currentThis = pageThis;
           }
-          args.length < 5
-            ? ofn.fn.call(currentThis, ...args)
-            : ofn.fn.apply(currentThis, args);
+          args.length < 5 ? lifeFn.fn.call(currentThis, ...args) : lifeFn.fn.apply(currentThis, args);
         }
       };
     }
   }
-};
+  return pageData;
+}
 
 /* 自定义Page */
 Page = data => {
-  let components = data.components;
-  if (!components || !Array.isArray(components) || !components.length) {
+  let { components } = data;
+  if (!Array.isArray(components) || !components.length) {
     return nativePage(data);
   }
-  if (typeof data.data !== "object") {
+  if (typeof data.data !== 'object') {
     data.data = Object.create(null);
   }
-  let cos = components.map(requireCo).filter(Boolean);
-  let c = cos.reduce(
-    (o, v) => {
-      v = { ...v };
-      if (!v.name) {
+  components = components.map(requireCo).filter(Boolean);
+  let registerData = components.reduce(
+    (data, componentData) => {
+      componentData = { ...componentData };
+      if (!componentData.name) {
         handlerError(
-          o.name,
-          "component",
-          `component must define unique name,but now get ${v.name} name`
+          data.name,
+          'component',
+          `component must define unique name,but now get ${componentData.name} name`
         );
       }
-      if (o[v.name]) {
+      if (data[componentData.name]) {
         handlerError(
-          o.name,
-          "page",
-          `page register duplicate ${v.name} component,Please make sure to be unique`
+          data.name,
+          'page',
+          `page register duplicate ${componentData.name} component,Please make sure to be unique`
         );
       }
-      if (v.data) {
-        o.data = { ...o.data, [v.name]: v.data };
+      if (componentData.data) {
+        data.data = { ...data.data, [componentData.name]: { ...componentData.data } };
       }
-      for (let key of protectedEvent) {
-        if (typeof v[key] === "function") {
-          let symbol = Symbol["for"](key);
-          !Array.isArray(o[symbol]) && (o[symbol] = []);
-          o[symbol].push({
-            fn: v[key],
-            type: "component",
-            context: v
+      for (let key of PROTECTEDEVENTS) {
+        if (typeof componentData[key] === 'function') {
+          let symbol = Symbol['for'](key);
+          !Array.isArray(data[symbol]) && (data[symbol] = []);
+          data[symbol].push({
+            fn: componentData[key],
+            type: 'component',
+            context: componentData
           });
         }
       }
-      let fns = Object.keys(v).filter(
-        vv => v.hasOwnProperty(vv) && typeof v[vv] === "function"
+      let otherFunctions = Object.keys(componentData).filter(
+        attrName => typeof componentData[attrName] === 'function' && !ownHas(PROTECTEDEVENTS, attrName)
       );
-      for (let fn of fns) {
-        o[`${v.name}.${fn}`] = function() {
-          let newThis = createComponentThis(v, this);
-          let args = Array.from(arguments);
-          args.length < 5
-            ? v[fn].call(newThis, ...args)
-            : v[fn].apply(newThis, args);
+      for (let fn of otherFunctions) {
+        data[`${componentData.name}.${fn}`] = function(...args) {
+          let newThis = createComponentThis(componentData, this);
+          args.length < 5 ? componentData[fn].call(newThis, ...args) : componentData[fn].apply(newThis, args);
         };
       }
-      o[v.name] = true;
-      return o;
+      data[componentData.name] = true;
+      return data;
     },
     { ...data }
   );
-  bindComponentLifeEvent(c);
-  nativePage(c);
+  nativePage(bindComponentLifeEvent(registerData));
 };
